f52df2a57e6f6ab1540233e416a9d21aa319b475
Ari Agafonova <agafonova_arina@icloud.com>
2026-05-14 15:05:35 +0200

    Initial Commit

---
+++ TripMate Lite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripMate Lite
+//
+//  Created by Арина Агафонова on 14.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
